fdcd1b49d4e48226bffd56e80a92d90c7ef0ae34
shakilahmedkhan <shakilcust@gmail.com>
2022-08-23 16:45:53 +0600

    before downloading zephyr we added model and migrated for students

--- students/views.py
+++ students/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 
 #function based views are declared here - khan
 
 def index(request):
-    return render(request,'students/index.html')
+    return render(request,'students/index.html', {
+
+        'students' : Student.objects.all()
+
+    })
 
